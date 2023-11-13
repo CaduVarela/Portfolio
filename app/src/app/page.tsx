918e8bff3d9bf92cce7718e9c2af320306e67389
Carlos Varela <caduvarela@gmail.com>
2023-11-13 14:41:00 -0300

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,10 @@
 import ReactQueryWrapper from "./components/reactquerywrapper/ReactQueryWrapper"
+import Index from "./components/index"
 
 const Page = () => {
     return (
         <ReactQueryWrapper>
-            
+            <Index />
         </ReactQueryWrapper>  
     )
 }
