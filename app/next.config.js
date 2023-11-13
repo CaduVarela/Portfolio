04af10a6ff6d6ffcee1e17329b680db654614930
Carlos Varela <caduvarela@gmail.com>
2023-11-13 15:04:02 -0300

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+
+    basePath: "/app",
+    assetPrefix: "/app",
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
