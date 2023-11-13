91b58db03e203af469f556fba58c46ade0a91d5a
Carlos Varela <caduvarela@gmail.com>
2023-11-13 14:38:30 -0300

    Creating Page.tsx

---
+++ app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import ReactQueryWrapper from "./components/reactquerywrapper/ReactQueryWrapper"
+
+const Page = () => {
+    return (
+        <ReactQueryWrapper>
+            
+        </ReactQueryWrapper>  
+    )
+}
+
+export default Page
